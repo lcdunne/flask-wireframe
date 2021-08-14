2a850d3f14d73f0710f233bcab95bfa0657f0a5d
lcdunne <lewis.dunne00@gmail.com>
2021-08-14 10:25:16 +0100

    Add test with fixture

--- tests/test_exmaple.py
+++ tests/test_exmaple.py
@@ -1,12 +1,18 @@
+import pytest
 from app import create_app
+from app.models import User
 
+
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(name='God', email_address='god@heaven.com')
+    return user
+
+def test_user(new_user):
+    assert new_user.name == 'God'
+    assert new_user.email_address == 'god@heaven.com'
 
 def test_home_page():
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET)
-    THEN check that the response is valid
-    """
     app = create_app('config.TestConfig')
 
     # Create a test client using the Flask application configured for testing
