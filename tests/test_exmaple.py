e32e3ec5d0610ba32f056fb4e1c88485b931acef
lcdunne <lewis.dunne00@gmail.com>
2021-08-14 10:34:19 +0100

    Update tests

--- tests/test_exmaple.py
+++ tests/test_exmaple.py
@@ -8,15 +8,18 @@ def new_user():
     user = User(name='God', email_address='god@heaven.com')
     return user
 
+@pytest.fixture(scope='module')
+def test_client():
+    app = create_app('config.TestConfig')
+    with app.test_client() as testing_client:
+        with app.app_context():
+            yield testing_client
+
 def test_user(new_user):
     assert new_user.name == 'God'
     assert new_user.email_address == 'god@heaven.com'
 
-def test_home_page():
-    app = create_app('config.TestConfig')
-
-    # Create a test client using the Flask application configured for testing
-    with app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
-        assert b"Hello" in response.data
+def test_home_page(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b"Hello" in response.data
